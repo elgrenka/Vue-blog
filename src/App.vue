f819e22e1b18047bfc9df6343841228f38fcc748
elgrenka <bugigen@yandex.ru>
2022-07-16 15:00:41 +0400

    w

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Create post</h4>
-      <input
-        class="input"
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        type="text"
-        placeholder="Name">
-      <input
-        class="input"
-        @input="body = $event.target.value"
-        type="text"
-        placeholder="Description"
-        v-bind:value="body">
-      <button class="btn" @click="createPost">Add post</button>
-    </form>
-    <div class="post" v-for="post of posts" v-bind:key=post.id>
-      <div><b>Name: </b>{{ post.title }}</div>
-      <div><b>Description: </b>{{ post.body }}</div>
-    </div>
+    <post-form
+      @create="createPost"
+    />
+    <post-list
+      :posts="posts"
+    />
   </div>
 </template>
 
 <script>
+import PostForm from './components/PostForm';
+import PostList from '@/components/PostList';
+
 export default {
+  components: {
+    PostForm, PostList
+  },
+
   data() {
     return {
       posts: [
@@ -32,20 +25,12 @@ export default {
         { id: 2, title: 'Javascript 2', body: 'Description of the post 2' },
         { id: 3, title: 'Javascript 3', body: 'Description of the post 3' },
       ],
-      title: '',
-      body: ''
     };
   },
+
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body
-      };
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post) {
+      this.posts.push(post);
     }
   }
 };
@@ -62,31 +47,5 @@ export default {
   padding: 20px;
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid blue;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: darkred;
-  border: 1px solid darkred;
-}
 </style>
 
---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post" v-for="post of posts" :key=post.id>
+    <div><b>Name: </b>{{ post.title }}</div>
+    <div><b>Description: </b>{{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      posts: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
+
+
+

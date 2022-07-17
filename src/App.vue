d5d99acaaa11799163594de883ef093a1523b5a2
elgrenka <bugigen@yandex.ru>
2022-07-17 12:22:43 +0400

    modal window

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="app">
-    <post-form
+    <h1>The page of posts</h1>
+    <my-button
+      style="margin: 15px 0; border: 2px solid orange; color: orange"
+      @click="showDialog"
+    >
+      Create post
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
       @create="createPost"
     />
+    </my-dialog>
     <post-list
       :posts="posts"
+      @remove="removePost"
     />
   </div>
 </template>
@@ -12,10 +22,13 @@
 <script>
 import PostForm from './components/PostForm';
 import PostList from '@/components/PostList';
+import MyDialog from './components/UI/MyDialog.vue';
 
 export default {
   components: {
-    PostForm, PostList
+    PostForm,
+    PostList,
+    MyDialog
   },
 
   data() {
@@ -25,12 +38,20 @@ export default {
         { id: 2, title: 'Javascript 2', body: 'Description of the post 2' },
         { id: 3, title: 'Javascript 3', body: 'Description of the post 3' },
       ],
+      dialogVisible: false,
     };
   },
 
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
     }
   }
 };
@@ -41,6 +62,7 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Comic Sans MS', cursive;
 }
 
 .app {
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <h3>List of users</h3>
-    <post-item
-      v-for="post of posts"
-      :post="post"
-      :key="post.id"
-    />
+    <h3>List of posts</h3>
+    <!-- <div v-if="posts.length > 0"> --> 
+    <div v-show="posts.length > 0">
+      <post-item
+        v-for="post of posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </div>
+    <h4
+      v-show="posts.length === 0"
+      style="color: crimson; margin-top: 15px"
+    >
+      ... is empty! Add the post!
+    </h4>
   </div>
 </template>
 
 <script>
-import PostItem from '@/components/PostItem';
+import PostItem from "@/components/PostItem";
 export default {
-  components: {PostItem},
+  components: { PostItem },
   props: {
     posts: {
       type: Array,
@@ -23,7 +33,6 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
 
 
